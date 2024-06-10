75c606f3c774be5f0174ab745dc7ab7aa7fec093
kaegan <kkiefer4@student.umgc.edu>
2024-06-11 08:36:28 +0900

    Submission 3

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task3;
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
         if (first % second == 0) {
-            System.out.println("Not Aliquot");
+            System.out.println("Not aliquot");
         } else {
             System.out.println("Aliquot");
         }
